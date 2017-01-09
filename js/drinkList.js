8a43e3e3217c7139aeec71cb683ffc630872aca5
Nathan Samano <nsv9982@saintjoe.edu>
2017-01-08 22:41:23 -0500

    added content and comments for Old Fashioned

--- js/drinkList.js
+++ js/drinkList.js
@@ -1,10 +1,13 @@
 /*
  * drinkList.js
  * JSON of all drink recipes
+ *
+ * List DRINKS alphabetically by name
  */
 
 var DRINKS = [
-  {name: "Old Fashioned", ingredients: [["Bourbon/Rye Whiskey", 1.5, "oz"], ["Angostura Bitters", 2, "dashes"], ["Sugar Cube", 1], "Water", 3, "dashes"],
+  {name: "Old Fashioned", ingredients: [["Bourbon/Rye Whiskey", 1.5, "oz"], ["Angostura Bitters", 2, "dashes"], ["Sugar Cube", 1, ""], ["Water", 3, "dashes"]],
+    garnish: ["Orange Twist", "Cocktail Cherry"],
     tags: ["orange", "strong", "cold", "lowball", "classic", "IBA Official"]}
 ];
 
--- js/ingredientList.js
+++ js/ingredientList.js
@@ -3,14 +3,16 @@
  * JSON of all ingredients
  * Necessary for ABV and calorie calculations
  *
+ * List INGREDIENTS alphabetically by name
+ *
  * Note: calories = calories per 1.5 ounce shot
- *    or calories = calories per dash (1/8 teaspoon)
+ *    or calories = calories per dash (1/8 teaspoon = 1/48 ounce)
  */
 
 var INGREDIENTS = [
+  {name: "Angostura Bitters", ABV:  0, calories:   2},
   {name: "Bourbon",           ABV: 40, calories:  97},
   {name: "Rye Whiskey",       ABV: 45, calories: 104},
-  {name: "Angostura Bitters", ABV:  0, calories:   4},
   {name: "Sugar Cube",        ABV:  0, calories:  12},
   {name: "Water",             ABV:  0, calories:   0}
 ];
